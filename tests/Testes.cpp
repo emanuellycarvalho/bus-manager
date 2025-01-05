2810203ea70922b7ec7761b1f510ce8f03bbcf9f
Emanuelly Carvalho <emanuellyvcarv@gmail.com>
2025-01-05 20:32:23 -0300

    Adicionando novos testes

--- tests/Testes.cpp
+++ tests/Testes.cpp
@@ -211,76 +211,29 @@ TEST(ViagemTest, OcuparAssentoSemAssentosDisponiveis) {
 
     Viagem viagem(&origem, &destino, &onibus, data);
     viagem.ocuparAssento();
-    ASSERT_THROW(viagem.ocuparAssento(), invalid_argument);
+    ASSERT_THROW(viagem.ocuparAssento(), overflow_error);
 }
 
-// Testes da classe Reserva
-
-TEST(ReservaTest, InicializarReservaComAtributosCorretos) {
+TEST(ViagemTest, desocuparAssentoComAssentoOcupado) {
     Onibus onibus("XYZ9876", 40, 60.0, 10.0);
     Parada origem("Origem", 0.0);
     Parada destino("Destino", 400.0);
     Data data(1, 1, 2024);
+
     Viagem viagem(&origem, &destino, &onibus, data);
-
-    Reserva reserva(&viagem, "12345678901", &origem, &destino);
-    ASSERT_EQ(reserva.getViagem()->getOrigem()->getNome(), "Origem");
-    ASSERT_EQ(reserva.getViagem()->getDestino()->getNome(), "Destino");
-    ASSERT_EQ(reserva.getViagem()->getOnibus()->getPlaca(), "XYZ9876");
-    ASSERT_EQ(reserva.getViagem()->getData().getAno(), 2024);
-    ASSERT_EQ(reserva.getViagem()->getData().getMes(), 1);
-    ASSERT_EQ(reserva.getViagem()->getData().getDia(), 1);
-    ASSERT_EQ(reserva.getCPF(), "12345678901");
-    ASSERT_DOUBLE_EQ(reserva.getValorPassagem(), 400.0 * 10.0);
-}
-
-TEST(ReservaTest, InicializarReservaComViagemNula) {
-    Parada origem("Origem", 0.0);
-    Parada destino("Destino", 400.0);
-    ASSERT_THROW(Reserva reserva(nullptr, "12345678901", &origem, &destino), invalid_argument);
-}
-
-TEST(ReservaTest, InicializarReservaComCPFNulo) {
-    Onibus onibus("XYZ9876", 40, 60.0, 10.0);
-    Parada origem("Origem", 0.0);
-    Parada destino("Destino", 400.0);
-    Data data(1, 1, 2024);
-    Viagem viagem(&origem, &destino, &onibus, data);
-
-    ASSERT_THROW(Reserva reserva(&viagem, "", &origem, &destino), invalid_argument);
-}
-
-TEST(ReservaTest, InicializarReservaComOrigemNula) {
-    Onibus onibus("XYZ9876", 40, 60.0, 10.0);
-    Parada origem("Origem", 0.0);
-    Parada destino("Destino", 400.0);
-    Data data(1, 1, 2024);
-    Viagem viagem(&origem, &destino, &onibus, data);
-
-    ASSERT_THROW(Reserva reserva(&viagem, "12345678901", nullptr, &destino), invalid_argument);
-}
-
-TEST(ReservaTest, InicializarReservaComDestinoNulo) {
-    Onibus onibus("XYZ9876", 40, 60.0, 10.0);
-    Parada origem("Origem", 0.0);
-    Parada destino("Destino", 400.0);
-    Data data(1, 1, 2024);
-    Viagem viagem(&origem, &destino, &onibus, data);
-
-    ASSERT_THROW(Reserva reserva(&viagem, "12345678901", &origem, nullptr), invalid_argument);
-}
-
-TEST(ReservaTest, CancelarReserva) {
-    Onibus onibus("XYZ9876", 40, 60.0, 10.0);
-    Parada origem("Origem", 0.0);
-    Parada destino("Destino", 400.0);
-    Data data(1, 1, 2024);
-    Viagem viagem(&origem, &destino, &onibus, data);
-
-    Reserva reserva(&viagem, "12345678901", &origem, &destino);
-    reserva.cancelarReserva();
+    viagem.ocuparAssento();
+    viagem.desocuparAssento();
     ASSERT_EQ(viagem.getNumAssentosLivres(), 40);
-    ASSERT_EQ(reserva.estaAtiva(), false);
+}
+
+TEST(ViagemTest, desocuparAssentoSemAssentoOcupado) {
+    Onibus onibus("XYZ9876", 40, 60.0, 10.0);
+    Parada origem("Origem", 0.0);
+    Parada destino("Destino", 400.0);
+    Data data(1, 1, 2024);
+
+    Viagem viagem(&origem, &destino, &onibus, data);
+    ASSERT_THROW(viagem.desocuparAssento(), invalid_argument);
 }
 
 int main(int argc, char **argv) {
